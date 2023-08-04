43febcf690aff8ce286f8cb4d6a2609eb05564db
Drey Smith <90289165+smithd36@users.noreply.github.com>
2023-08-03 22:11:15 -0600

    Push portfolio section updates

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -8,7 +8,6 @@ import work1 from '../../assets/images/work-1.png';
 import work2 from '../../assets/images/work-2.png';
 import work3 from '../../assets/images/work-3.png';
 
-
 const Portfolio = () => {
   const [letterClass, setLetterClass] = useState('text-animate');
 
@@ -19,24 +18,24 @@ const Portfolio = () => {
   }, []);
 
   return (
-    <>
-      <div className="container portfolio-page">
-        <div className="text-zone">
-          <h1>
-            <AnimatedLetters
-              letterClass={letterClass}
-              strArray={['C', 'u', 'r', 'r', 'e', 'n', 't', ' ', 'P', 'r', 'o', 'j', 'e', 'c', 't', 's']}
-              idx={15}
-            />
-          </h1>
-          <div className="portfolio-container">
-            <div className="portfolio-item">
+    <div className="portfolio-page">
+      <div className="text-zone">
+        <h1>
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={['C', 'u', 'r', 'r', 'e', 'n', 't', ' ', 'P', 'r', 'o', 'j', 'e', 'c', 't', 's']}
+            idx={15}
+          />
+        </h1>
+        <div className="portfolio-container">
+          {/* Portfolio items */}
+          <div className="portfolio-item">
               <img src={work1} alt="work1" />
               <div className="portfolio-item-text">
                 <h3>MediGuard Licensure</h3>
                 <p>A full-stack MySQL (Azure hosted) Next.js tool designed to efficiently handle licensure of first responders in a location. This application simplifies administrative tasks, allowing healthcare professionals to focus on patient care. Mediguard Licensure is currently being used in the industry (client-name kept private) and I am continuing to scale and improve this application consistently, emphasising compliance and security.</p>
                 {/* Tech used displaying logos for each */}
-                <span className="tech-stack"><p><b>Azure | MySQL</b></p><span id="icons"><FontAwesomeIcon icon={faPython} color="red" /><FontAwesomeIcon icon={faNode} color="green" /><FontAwesomeIcon icon={faReact} color="aqua" /><FontAwesomeIcon icon={faJs} color="#FFFF00" /><FontAwesomeIcon icon={faSass} color="hotpink" /></span></span>
+                <span className="tech-stack"><p><b>Azure | MySQL</b></p><span className="bigger-icons"><FontAwesomeIcon icon={faPython} color="red" /><FontAwesomeIcon icon={faNode} color="green" /><FontAwesomeIcon icon={faReact} color="aqua" /><FontAwesomeIcon icon={faJs} color="#FFFF00" /><FontAwesomeIcon icon={faSass} color="hotpink" /></span></span>
                 <span>Project Repository:<a id= "gh-link" rel="noreferrer" target="_blank" href="https://github.com/smithd36/licensure-management-nextjs"><FontAwesomeIcon icon={faGithub} /></a></span>
               </div>
             </div>
@@ -54,19 +53,18 @@ const Portfolio = () => {
               <img src={work3} alt="work3" />
               <div className="portfolio-item-text">
                 <h3>C++ Powershell Toolkit</h3>
-                <p>This innovative shell interface takes command execution to the next level by seamlessly managing multiple user commands in parallel processes. Effortlessly orchestrate command pairs with intuitive pipes for seamless communication and data flow. Redefine output destinations with precision and navigate complex workflows efficiently. Our user-centric design supports commands with arguments, ensuring a tailored experience. With a robust architecture and comprehensive documentation, the CS 461 OS Shell is your gateway to a more powerful and efficient command line experience.</p>
+                <p>A C++ shell interface that seamlessly manages multiple user commands in parallel processes. Effortless command pairs with pipes for reliable communication and data flow. Redefine output destinations with precision and navigate complex workflows efficiently. A user-centric design that supports commands with arguments, ensuring a tailored experience.</p>
                 {/* Tech used displaying logos for each */}
                 <span className="tech-stack"><p><b>College Project</b></p><span id="icons"><FontAwesomeIcon icon={faGit} color="orange" /><FontAwesomeIcon icon={faWindows} color="blue" /><FontAwesomeIcon icon={faLinux} color="#FF4500" /><FontAwesomeIcon icon={faJs} color="#FFFF00" /></span></span>
                 <span>Project Repository:<a id= "gh-link" rel="noreferrer" target="_blank" href="https://github.com/smithd36/unix-power-shell"><FontAwesomeIcon icon={faGithub} /></a></span>
               </div>
             </div>
-          </div>
-          <p id="header-caption">Skills grow. So will this portfolio.</p>
-          <p><i>Full Project Archive Coming Soon...</i></p>
         </div>
+        <p id="header-caption">Skills grow. So will this portfolio.</p>
+        <p><i>Full Project Archive Coming Soon...</i></p>
       </div>
       <Loader type="pacman" />
-    </>
+    </div>
   );
 };
 
